6b7049b1f2a69d5e30f4c06de8af49292b829bcc
Kazuch, Sebastian <sebastian.kazuch@sap.com>
2019-06-06 09:24:15 +0200

    Reverting update to newest spartacus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ConfigModule } from '@spartacus/core';
 import { AppComponent } from './app.component';
-import { StorefrontModule, defaultCmsContentConfig } from '@spartacus/storefront';
+import { StorefrontModule ,defaultCmsContentConfig,translations} from '@spartacus/storefront';
 import { ReviewProductPopupComponent } from './review-product-popup/review-product-popup.component';
-import { translations } from '@spartacus/assets';
+
 import { PandamonsterProductReviewComponent } from './pandamonster-product-review/pandamonster-product-review.component';
 
 import { CommonModule } from '@angular/common';
@@ -39,37 +39,36 @@ import {
         },
       },
       siteContext: {
-        urlEncodingParameters: ['BASE_SITE', 'LANGUAGE', 'CURRENCY'],
-        parameters: {
-          BASE_SITE: {
-            values: ['electronics-spa', 'electronics', 'apparel-de', 'apparel-uk'],
-            defaultValue: 'electronics-spa',
-            persistence: 'route'
+          urlEncodingParameters: ['BASE_SITE', 'LANGUAGE', 'CURRENCY'],
+          parameters: {
+            BASE_SITE: {
+              values: ['electronics-spa', 'electronics', 'apparel-de', 'apparel-uk'],
+              defaultValue: 'electronics-spa',
+              persistence: 'route'
+            }
           }
-        }
-      },
+        },
       backend: {
-
+        
         occ: {
           baseUrl: 'https://api.cqz1m-softwarea1-d49-public.model-t.cc.commerce.ondemand.com',
-          //prefix: '/rest/v2/electronics-spa',
-          legacy: true
+          prefix: '/rest/v2/'
+        },
+        authentication: {
+          client_id: 'mobile_android',
+          client_secret: 'secret'
+        },
+        site: {
+          //baseSite: 'electronics-spa'
+          
+        
+        },
+        
+        icon: {
+          prefix: 'fa-',
+          iconClass: 'fas'
         }
       },
-      authentication: {
-        client_id: 'mobile_android',
-        client_secret: 'secret'
-      },
-      site: {
-        baseSite: 'electronics-spa'
-
-      },
-
-      icon: {
-        prefix: 'fa-',
-        iconClass: 'fas'
-      }
-      ,
       i18n: {
         resources: translations
       }
